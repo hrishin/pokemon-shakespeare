9fdbf68b50305940e70708105304cfd27c078ebf
hrishin <hrishikesh.shinde@productmadness.com>
2020-11-28 19:24:48 +0000

    Initial translation API integration - This patch integrates the funtranslations to get Shakesspeares words for the given prahse. - It also adds some API tests

---
+++ pkg/translation/api.go
@@ -0,0 +1,113 @@
+package translation
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+)
+
+type response struct {
+	Success struct {
+		Total int `json:"total"`
+	} `json:"success"`
+	Contents struct {
+		Translated string `json:"translated"`
+	} `json:"contents"`
+}
+
+type errorResponse struct {
+	Error struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	} `json:"error"`
+}
+
+type ServiceResponse struct {
+	Content  string
+	Error    error
+	ErroCode int
+}
+
+func NewResponseError(err error) *ServiceResponse {
+	return &ServiceResponse{
+		Error: err,
+	}
+}
+
+func NewResponseErrorCode(code int, err error) *ServiceResponse {
+	return &ServiceResponse{
+		ErroCode: code,
+		Error:    err,
+	}
+}
+
+func NewRespnse(content string) *ServiceResponse {
+	return &ServiceResponse{Content: content}
+}
+
+type API struct {
+	Client *http.Client
+	URL    string
+	Key    string
+}
+
+func NewAPI(APIKey string) *API {
+	return &API{
+		Client: &http.Client{},
+		URL:    "https://api.funtranslations.com/translate/",
+		Key:    APIKey,
+	}
+}
+
+func (a *API) rquestShakespeare(text string) (*http.Request, error) {
+	data := map[string]string{"text": text}
+	post, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, a.URL+"shakespeare.json", bytes.NewBuffer(post))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	return req, nil
+}
+
+func (a *API) Translate(text string) *ServiceResponse {
+	req, err := a.rquestShakespeare(text)
+	if err != nil {
+		return NewResponseError(err)
+	}
+
+	resp, err := a.Client.Do(req)
+	if err != nil {
+		return NewResponseError(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return NewResponseError(err)
+	}
+
+	if resp.StatusCode >= 400 {
+		var errorResponse errorResponse
+		err := json.Unmarshal(body, &errorResponse)
+		if err != nil {
+			return NewResponseError(err)
+		}
+		return NewResponseErrorCode(resp.StatusCode, errors.New(errorResponse.Error.Message))
+	}
+
+	var response response
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return NewResponseError(err)
+	}
+
+	return NewRespnse(response.Contents.Translated)
+}
