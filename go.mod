4568eae362934b0cdc2e5e84d23679acdad4e483
hrishin <hrishikesh.shinde@productmadness.com>
2020-11-29 21:49:48 +0000

    Adds endpoint tests

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/onsi/ginkgo v1.14.2
+	github.com/onsi/gomega v1.10.3
 	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
 )
--- tests/pokemon_test.go
+++ tests/pokemon_test.go
@@ -1,49 +1,13 @@
-package pokemon
+package tests
 
 import (
-	"net/http"
-	"net/http/httptest"
-	"testing"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 
-	"github.com/gorilla/mux"
+	"testing"
 )
 
-func Test_get_description(t *testing.T) {
-	tt := []struct {
-		name         string
-		endpoint     string
-		pokmon       string
-		wantStatus   int
-		wantResponse string
-	}{
-		{
-			name:       "for charizard",
-			endpoint:   "/pokemon/",
-			pokmon:     "charizard",
-			wantStatus: 200,
-		},
-		{
-			name:       "for a invalid pokemon",
-			endpoint:   "/pokemon/",
-			pokmon:     "foobar",
-			wantStatus: 404,
-		},
-	}
-
-	for _, tc := range tt {
-		t.Run(tc.name, func(t *testing.T) {
-			req, err := http.NewRequest(http.MethodGet, tc.endpoint+tc.pokmon, nil)
-			if err != nil {
-				t.Fatal(err)
-			}
-			router := mux.NewRouter()
-			router.HandleFunc(tc.endpoint+"{name}", GetDescriptionHandler)
-			rr := httptest.NewRecorder()
-			router.ServeHTTP(rr, req)
-
-			if status := rr.Code; status != tc.wantStatus {
-				t.Errorf("unexpected error code returned: got %v want %v", status, http.StatusOK)
-			}
-		})
-	}
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Test Suite")
 }
